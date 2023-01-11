e9049e16426321909ad176a581eab80c4fe9de1f
ikalli <andoune0719@gmail.com>
2023-01-11 11:28:20 +0900

    🐛 Bugfix - build error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Dogu docs',
   tagline: 'Automate your game QA',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.dogutech.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -23,8 +23,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'ko',
+    locales: ['ko'],
   },
 
   presets: [
