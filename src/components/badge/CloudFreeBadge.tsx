4bb63ef5d8e000ae484d4cb7da296a3035d9b454
Seong JuWon <lumyjuwon@gmail.com>
2023-10-18 14:44:18 +0900

    init docs

---
+++ src/components/badge/CloudFreeBadge.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Badge from './Badge';
+
+const CloudFreeBadge = () => {
+  return (
+    <Badge
+      label={'Cloud'}
+      overlayText={'Available in the cloud service'}
+      backgroundColor={'#5ba2e9'}
+    />
+  );
+};
+
+export default CloudFreeBadge;
